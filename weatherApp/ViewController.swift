9a5c06338d34db3d33b3a55dc219b4af12dabe90
Ahljun Ciar <ahljun@192-168-1-104.tpgi.com.au>
2017-11-28 14:21:12 +1100

    Initial Commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Ahljun Ciar on 28/11/17.
+//  Copyright © 2017 Ahljun Ciar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
